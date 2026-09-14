87098d6d3707cfdee4928b3ed8b3a33f4e51b037
Ryodai Toda <ryou.0205dai@gmail.com>
2026-09-14 12:47:53 +0900

    env

---
+++ build/_deps/raylib-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ryou0/OneDrive - OUMail (The University of Osaka)/プログラミング/3D_CPP/test/build/_deps/raylib-src
+# Build directory: C:/Users/ryou0/OneDrive - OUMail (The University of Osaka)/プログラミング/3D_CPP/test/build/_deps/raylib-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
---
+++ build/_deps/raylib-build/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ryou0/OneDrive - OUMail (The University of Osaka)/プログラミング/3D_CPP/test/build/_deps/raylib-src/src
+# Build directory: C:/Users/ryou0/OneDrive - OUMail (The University of Osaka)/プログラミング/3D_CPP/test/build/_deps/raylib-build/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
